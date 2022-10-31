410623655330114648ff53949340fdcbf7328ad4
Niklas Stephenson <niklas@stephenson.dk>
2022-10-31 16:31:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ npx tailwindcss -i ./source/stylesheets/site.css -o ./source/stylesheets/out.css
 
 middleman build
 
+echo "Moving files to Synology"
 cp -R build/* /Volumes/Niklas/web/www.stephenson.dk
+
+echo "Deploy done"
