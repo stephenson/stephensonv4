cc91cf4fe807707a5dea41380c9b29d2422d9790
Niklas Stephenson <niklas@stephenson.dk>
2024-07-23 13:52:53 +0200

    New deploy triggered

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@ npx tailwindcss -i ./source/stylesheets/site.css -o ./source/stylesheets/out.css
 
 middleman build
 
-mv /build /public
-
 git commit -a -m "New deploy triggered"
 
 git push origin master
